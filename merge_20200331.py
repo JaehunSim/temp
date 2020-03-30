53924ac7bea5092fef789f9d68edc3f4d38d8e81
Jaehun Sim <33471819+JaehunSim@users.noreply.github.com>
2020-03-31 08:04:38 +0900

    Update merge_20200331.py

--- merge_20200331.py
+++ merge_20200331.py
@@ -8,4 +8,4 @@ ncc2 = pd.DataFrame([[0.2, 0.5, 0.4, 0.6], [0.8, 0.9, 0.2, 0.1], [0.5, 0.7, 0.9,
                     index=["A", "B", "C", "D"],
                     columns=[5, 6, 7, 8])
 
-data = pd.merge(ncc2, ncc1, how="outer", left_index=True, right_index=True)
+data = pd.merge(ncc2, ncc1, how="outer", left_index=True, right_index=True)```
